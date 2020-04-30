2f76cffd3f31d8bb4c99b88f035a616520e767e4
Emilio <e.bondioli@todo.to.it>
2020-04-30 22:01:05 +0200

    include new graphs

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,29 @@
 <template>
   <div class="home">
-    <TotalCallouts />
-    <TimeSeries v-if="ready" />
+    <section class="intro">
+      <h1 class="expa-large">Wish i were there</h1>
+      <p>Self-isolating is the right thing to do during the pandemic. But it also makes our favorite places feel like a constellation of faraway stars, and we wish we could just think about the future and already be there. Stay safe and we will get there.</p>
+    </section>
+    <TotalCallouts class="callouts" />
+    <TimeSeries />
+    <h2>New users this week</h2>
+    <div class="row">
+      <LatestUsers class="col-4" />
+      <GrowthStream class="col-8" />
+    </div>
   </div>
 </template>
 
 <script>
 import SingleLoader from "@/components/SingleLoader";
-import TimeSeries from "@/components/TimeSeries/";
-import TotalCallouts from "@/components/TotalCallouts/";
+import TimeSeries from "@/components/TimeSeries";
+import TotalCallouts from "@/components/TotalCallouts";
+import LatestUsers from "@/components/LatestUsers";
+import GrowthStream from "@/components/GrowthStream";
 
 export default {
   name: "Home",
-  components: { Loader: SingleLoader, TimeSeries, TotalCallouts },
+  components: { Loader: SingleLoader, TimeSeries, TotalCallouts, LatestUsers, GrowthStream },
   computed: {
     ready() {
       return this.$store.state.ready;
@@ -23,7 +34,25 @@ export default {
 
 <style lang="scss">
 .home {
-  padding: 1rem;
-  width: 1260px;
+  padding: 2rem;
+  width: 100%;
+}
+.callouts {
+  width: 50%;
+  margin: 0 auto;
+}
+.time-series {
+  margin-bottom: 2rem;
+}
+.intro {
+  text-align: center;
+  margin-bottom: 2rem;
+  h1 {
+    margin-bottom: 0.5rem;
+  }
+  p {
+    width: 70%;
+    margin: 0 auto;
+  }
 }
 </style>
