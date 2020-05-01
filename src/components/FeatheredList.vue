11558342c3c3c02ccaed4fd4829a42a0a54b5772
Emilio <e.bondioli@todo.to.it>
2020-05-01 22:53:33 +0200

    create dedicated component for feathered lists

---
+++ src/components/FeatheredList.vue
@@ -0,0 +1,27 @@
+<template>
+  <section class="feathered-list">
+    <div class="blur"></div>
+    <div class="items">
+      <slot></slot>
+    </div>
+    <div class="blur bottom"></div>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.feathered-list {
+  user-select: none;
+  position: relative;
+  height: 100%;
+  .items {
+    height: 100%;
+    overflow: auto;
+    padding: 8% 0;
+    mask-image: linear-gradient(to bottom, transparent 0%, black 10%, black 90%, transparent 100%);
+  }
+}
+</style>
